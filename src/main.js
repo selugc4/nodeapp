fe25e01c797ec863815c18f614ec171e1270f672
Jose Luis Garrido Castro <jgc450@inlumine.ual.es>
2025-04-17 20:33:58 +0200

    actualized main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening on port ${port}`)
 })
