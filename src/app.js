cfe313a5d6ac3e7b68b2b83f5fe2e60e23fde30b
Jose Luis Garrido Castro <jgc450@inlumine.ual.es>
2025-04-17 20:18:41 +0200

    app actualized

--- src/app.js
+++ src/app.js
@@ -10,5 +10,8 @@ app.get('/', (req, res) => {
 app.get('/:nameToSalute', (req, res) => {
   res.send(new HelloWordService().greet(req.params.nameToSalute));
 })
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
 module.exports = app
